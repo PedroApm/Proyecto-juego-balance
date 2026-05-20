d8b14c82f0836d30e1ae23157a154b86ee083328
PedroApm <146042938+PedroApm@users.noreply.github.com>
2026-05-19 22:02:36 -0500

    url  con /+esm

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const SUPABASE_URL = "https://pckkopvifrnbzzapmiyb.supabase.co"
 const SUPABASE_ANON_KEY = "sb_publishable_wceBqsl7CVdEyGP_hRm_Zg_lv7Ixz3k"
