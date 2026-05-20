ba5988a64a8475bbd9a5b85acbf6d6a86c46d76f
Santiago Mancilla <s.mancillag22@gmail.com>
2026-05-19 20:36:09 -0500

    agregar credenciales de Supabase y configurar cliente

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
 
 const SUPABASE_URL = "https://pckkopvifrnbzzapmiyb.supabase.co"
-const SUPABASE_ANON_KEY = "TU_PUBLISHABLE_KEY_AQUI"
+const SUPABASE_ANON_KEY = "sb_publishable_wceBqsl7CVdEyGP_hRm_Zg_lv7Ixz3k" // Conexión configurada por Equipo
 
 window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
